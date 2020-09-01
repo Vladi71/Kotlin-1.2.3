dfd034b4dd6d972b3568963e6d6b39c165000745
Vladi71 <ronrs6@mail.ru>
2020-09-01 22:47:16 +0300

    Total and Purchase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,26 @@
 package ru.netology
 
 fun main() {
-    val price = 100
-    val track = 90
+    val total = 12_000
+    val purchase = 1000
     val minDiscount = 0.03
     val maxDiscount = 0.05
     val musicLoverDiscount = 0.01
     val minPriceStartDiscount = 1000
     val maxPriceStartDiscount = 10_000
-    val musicLover = true
+    val musicLover = false
 
-    val totalPrice = price * track
-    val resultMinDiscount: Int = (totalPrice * minDiscount).toInt()
-    val resultMaxDiscount: Int = (totalPrice * maxDiscount).toInt()
-    val resultMusicLoverDiscount: Int = (totalPrice * musicLoverDiscount).toInt()
-    val result = if (musicLover && totalPrice in minPriceStartDiscount until maxPriceStartDiscount)
-        totalPrice - resultMusicLoverDiscount - resultMinDiscount
-    else if (musicLover && totalPrice >= maxPriceStartDiscount)
-        totalPrice - resultMusicLoverDiscount - resultMaxDiscount
-    else if (totalPrice in minPriceStartDiscount until maxPriceStartDiscount) totalPrice - resultMinDiscount
-    else if (totalPrice >= maxPriceStartDiscount) totalPrice - resultMaxDiscount
-    else totalPrice
+
+    val resultMinDiscount: Int = (purchase * minDiscount).toInt()
+    val resultMaxDiscount: Int = (purchase * maxDiscount).toInt()
+    val resultMusicLoverDiscount: Int = (purchase * musicLoverDiscount).toInt()
+    val result = if (musicLover && total in minPriceStartDiscount until maxPriceStartDiscount)
+        purchase - resultMusicLoverDiscount - resultMinDiscount
+    else if (musicLover && total >= maxPriceStartDiscount)
+        purchase - resultMusicLoverDiscount - resultMaxDiscount
+    else if (total in minPriceStartDiscount until maxPriceStartDiscount) purchase - resultMinDiscount
+    else if (total >= maxPriceStartDiscount) purchase - resultMaxDiscount
+    else purchase
 
     println("Спасибо за покупку! Сумма с учетом скидки составила $result руб.")
 
