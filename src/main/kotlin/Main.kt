859c72c9fe4f06c46069125f43e396cd0c85381e
Vladi71 <ronrs6@mail.ru>
2020-09-02 17:53:39 +0300

    Music Lover

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,25 +2,25 @@ package ru.netology
 
 fun main() {
     val total = 12_000
-    val purchase = 1000
+    val purchase = 101
     val minDiscount = 0.03
     val maxDiscount = 0.05
     val musicLoverDiscount = 0.01
     val minPriceStartDiscount = 1000
     val maxPriceStartDiscount = 10_000
-    val musicLover = false
+    val musicLover = true
 
 
-    val resultMinDiscount: Int = (purchase * minDiscount).toInt()
-    val resultMaxDiscount: Int = (purchase * maxDiscount).toInt()
-    val resultMusicLoverDiscount: Int = (purchase * musicLoverDiscount).toInt()
-    val result = if (musicLover && total in minPriceStartDiscount until maxPriceStartDiscount)
-        purchase - resultMusicLoverDiscount - resultMinDiscount
-    else if (musicLover && total >= maxPriceStartDiscount)
-        purchase - resultMusicLoverDiscount - resultMaxDiscount
+    val resultMinDiscount = purchase * minDiscount
+    val resultMaxDiscount = purchase * maxDiscount
+
+    val result: Double = if (musicLover && total in minPriceStartDiscount until maxPriceStartDiscount)
+        purchase - resultMinDiscount - (purchase * musicLoverDiscount)
+    else (if (musicLover && total >= maxPriceStartDiscount)
+        purchase  - resultMaxDiscount - (purchase * musicLoverDiscount)
     else if (total in minPriceStartDiscount until maxPriceStartDiscount) purchase - resultMinDiscount
     else if (total >= maxPriceStartDiscount) purchase - resultMaxDiscount
-    else purchase
+    else purchase) as Double
 
     println("Спасибо за покупку! Сумма с учетом скидки составила $result руб.")
 
